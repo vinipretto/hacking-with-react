b1985e8cbcf9838fc4784d96021ebbe3d8af1fd9
vinicius-pretto <vini.prettodasilva@gmail.com>
2016-07-03 19:55:17 -0300

    Adding List Component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
 import Detail from './pages/Detail';
+import List from './pages/List';
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactDOM.render(
   <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
-    <Route path='/' component={ Detail }></Route>
+    <Route path='/' component={ List }></Route>
+    <Route path='/react' component={ Detail }></Route>
   </Router>,
   document.getElementById('app')
 );
