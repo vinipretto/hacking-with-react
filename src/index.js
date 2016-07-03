abad9697209dfe1cd0265c295f92fcec7ea6a96f
vinicius-pretto <vini.prettodasilva@gmail.com>
2016-07-03 19:40:14 -0300

    Adding React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import Detail from './pages/Detail';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render(
-  <Detail />,
+  <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    <Route path='/' component={ Detail }></Route>
+  </Router>,
   document.getElementById('app')
 );
